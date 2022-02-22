47f9f42f6aedf26e4475f6681831eaeafeabae14
Allison <amh03160@gmail.com>
2022-02-22 16:38:57 -0600

    commit tree 74f3976864f5183184ee266ba6e5ae889fde6a70

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 19
+const num4 = 5
+//multiply 2 numbers
+const multiply = num1*num2;
+//random, integer greater than zero
+const random = Math.floor(Math.random()*10+1);
+
+//mod, calculates remainder 
+const mod = num3%num4;
+
+//max
+const max = Math.max(1,3,5,7,9,20);
